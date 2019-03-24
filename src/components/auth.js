cc01f2dcb46cca1d71c73f3392b24cfdd695a56b
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-03-24 22:26:55 +0300

    Add jwt verification

--- src/components/auth.js
+++ src/components/auth.js
@@ -3,11 +3,12 @@ import { CLIENT_ID, APP_URL } from '../keys';
 
 export default class Auth extends React.PureComponent {
   render() {
+
     return (
       <div>
         Auth
         <br />
-          <a href={`https://${APP_URL}/login?response_type=code&client_id=${CLIENT_ID}&redirect_uri=http://localhost:3000`}>
+          <a href={`https://${APP_URL}/login?response_type=token&client_id=${CLIENT_ID}&redirect_uri=http://localhost:3000`}>
           <button>
             Sign In
           </button>
--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,19 @@
 import * as React from 'react';
+import jwt from 'jsonwebtoken';
+import KJUR from 'jsrsasign';
 
 export default class Home extends React.PureComponent {
   render() {
+
+    let jwt = '';
+    if (this.props.location.hash) {
+      jwt = this.props.location.hash.slice(10);
+    }
+
+    var headerObj = KJUR.jws.JWS.readSafeJSONString(KJUR.b64utoutf8(KJUR.sJWT.split(".")[0]));
+    var payloadObj = KJUR.jws.JWS.readSafeJSONString(KJUR.b64utoutf8(KJUR.sJWT.split(".")[1]));
+
+    debugger
     return (
       <div>
         Home
